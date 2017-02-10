16a543a70dbb48490ef1919cc60527f56ae49045
Eirik Wiig <Eirikwiig@gmail.com>
2017-02-10 17:49:23 +0000

    Add user login test and home controller

---
+++ spec/features/trip_feature_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+feature 'Trip' do
+  scenario 'Sign up' do
+
+  end
+
+  scenario 'A user can create a new trip' do
+
+  end
+end
---
+++ spec/features/user_feature_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+feature "User can sign in and out" do
+  context "user not logged in and on the homepage" do
+    scenario "should see a 'login' link and a 'sign up' link" do
+      visit('/')
+      expect(page).to have_link('Log in')
+      expect(page).to have_link('Sign up')
+    end
+
+    scenario "should not see 'Sign out' link" do
+      visit('/')
+      expect(page).not_to have_link('Sign out')
+    end
+  end
+
+  context 'user logged in on the homepage' do
+    scenario "should see 'Salir' link" do
+      sign_up
+      expect(page).to have_link('Sign out')
+      expect(page).not_to have_link('Sign up')
+      expect(page).not_to have_link('Log in')
+    end
+  end
+end
+
+def sign_up(name: 'Name', email: 'test@example.com', password: 'testtest')
+  visit('/')
+  click_link('Sign up')
+  fill_in('user_name', with: name)
+  fill_in('user_email', with: email)
+  fill_in('user_password', with: password)
+  click_button('Submit')
+end
+
+def login_approved_user_factory_girl
+  user = FactoryGirl.create(:user, :approved)
+  login_as(user, scope: :user)
+  visit('/')
+end
+
