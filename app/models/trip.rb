f4fd3925ee14ea98478de5dc3e7ad258ed5043e9
Eirik Wiig <Eirikwiig@gmail.com>
2017-04-04 21:09:57 +0200

    [TR-14] Add cancan and trip authorisation
    Only a participant can see the trip, if not, access is denied.

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,5 +1,6 @@
 class Trip < ApplicationRecord
   validates :name, presence: true
+  validates :organiser, presence: true
   belongs_to :organiser, class_name: "User", foreign_key: "user_id"
   has_many :trip_participants
   has_many :participants, through: :trip_participants, source: :user
